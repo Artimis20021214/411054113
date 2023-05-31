5bcba4cce756046dbc45f1dc7ef8b00be307c855
Firstname Lastname <your@email.address>
2023-05-31 10:31:06 +0800

    1

--- app/src/main/java/com/example/a4110/MainActivity.kt
+++ app/src/main/java/com/example/a4110/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.a4110
 
+import android.content.pm.ActivityInfo
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a4110.ui.theme.張予綸4110Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContent {
             張予綸4110Theme {
                 // A surface container using the 'background' color from the theme
@@ -31,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello world")
+    Text(text = "Hello world ")
 }
 
 @Preview(showBackground = true)

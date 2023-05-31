709d05c27e1b410ecf8065628ff671a7b47331df
Firstname Lastname <your@email.address>
2023-05-31 11:35:34 +0800

    V.2

--- app/src/main/java/com/example/a4110/MainActivity.kt
+++ app/src/main/java/com/example/a4110/MainActivity.kt
@@ -5,21 +5,25 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a4110.ui.theme.張予綸4110Theme
 
 class MainActivity : ComponentActivity() {
@@ -42,13 +46,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column {
+
+    Column( modifier = Modifier.fillMaxSize(),verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.CenterHorizontally ) {
         Text(text = "資管2B 張予綸 ")
         Image(
 
             painter = painterResource(id = R.drawable.map),
             contentDescription = "地圖",
+
+
         )
+    }
+    Box(
+
+        modifier = Modifier.fillMaxSize()
+
+    ){
+
+        Canvas(modifier = Modifier){
+            drawRect(color = androidx.compose.ui.graphics.Color.Blue, Offset(700f,100f),Size(40f,40f)
+            )
+
+        }
+
     }
 }
 
